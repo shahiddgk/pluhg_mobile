4501ab01138800208b1d5dc0463e898a4d889455
Edward Victorhez <48755413+victorhez@users.noreply.github.com>
2021-11-18 20:38:10 +0100

    codebase set up

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color pluhgColour = const Color(0xff000BFF);
+Color pluhgYellowColour = const Color(0xffD8B831);
+Color pluhgMilkColour = const Color(0xffE5E5E5);
+Color pluhgGreenColour = const Color(0xff18C424);
+Color pluhgGrayColour = const Color(0xffA6A6A6);
+Color pluhgMenuGrayColour = const Color(0xff7F8C8D);
+Color pluhgMenuBlackColour = const Color(0xff080F18);
+Color pluhgRedColour = const Color(0xffFF5652);
---
+++ lib/features/onboarding/presentation/screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:pluhg/core/styles/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: pluhgColour,
+        body: Center(
+            child: SvgPicture.asset("assets/svgs/onboarding/logo_text.svg")));
+  }
+}
