1aa285ed6a5feacc4aa9a993243b414d3e18b650
Antoine Wachowiak <jihad.atlagh.1@gmail.com>
2022-01-21 11:34:29 +0100

    one design change

--- lib/widgets/pluhg_by_widget.dart
+++ lib/widgets/pluhg_by_widget.dart
@@ -11,34 +11,35 @@ class PlugByWidgetCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 100.w,
+        width: MediaQuery.of(context).size.width < 375 ? 84.w : 100.w,
         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      SizedBox(
-        height: 9.4,
-      ),
-      Text("Plugged by:", style: style_title),
-      Container(
-        height: 4.h,
-      ),
-      Container(child: Text(userName.toString(),
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          style: subtitleTextStyle)),
-      SizedBox(height: 8.71.h),
-      Text("Date:", style: style_title),
-      Container(
-        height: 4.h,
-      ),
-      Text(date.toString().substring(0, 11), style: subtitleTextStyle),
-      SizedBox(height: 8.71.h),
-      Text("Time:", style: style_title),
-      Container(
-        height: 4.h,
-      ),
-      Text(
-        date.toString().substring(12),
-        style: subtitleTextStyle,
-      ),
-    ]));
+          SizedBox(
+            height: 9.4,
+          ),
+          Text("Plugged by:", style: style_title),
+          Container(
+            height: 4.h,
+          ),
+          Container(
+              child: Text(userName.toString(),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: subtitleTextStyle)),
+          SizedBox(height: 8.71.h),
+          Text("Date:", style: style_title),
+          Container(
+            height: 4.h,
+          ),
+          Text(date.toString().substring(0, 11), style: subtitleTextStyle),
+          SizedBox(height: 8.71.h),
+          Text("Time:", style: style_title),
+          Container(
+            height: 4.h,
+          ),
+          Text(
+            date.toString().substring(12),
+            style: subtitleTextStyle,
+          ),
+        ]));
   }
 }
