c424fe1376f902bfb51cfe7008b97221d27f45d4
HimanshuGandhi <himanshu.g@crestinfosystems.net>
2022-04-07 09:09:58 +0530

    Clear Read message

--- lib/app/modules/chat_screen/views/chat_screen_view.dart
+++ lib/app/modules/chat_screen/views/chat_screen_view.dart
@@ -52,7 +52,32 @@ class ChatScreenView extends GetView<ChatScreenController> {
                             name_receiver: controller.users[i].name,
                             profile_receiver: APICALLS.imageBaseUrl + controller.users[i].profileImage!,
                             senderId: controller.users[i].senderId,
-                            recevierId: controller.users[i].recevierId));
+                            recevierId: controller.users[i].recevierId,
+                            clearUnReadMessageCount: (){
+
+
+                              print('CALL BACK CALL${controller.total_unread_messages.value}');
+
+                              var tempUser = controller.users[i];
+                              var temp = tempUser.unReadCount;
+
+                              tempUser.unReadCount = 0;
+                              var index = controller.users.removeAt(i);
+                              controller.users.insert(i, tempUser);
+                              //controller.users[i].unReadCount = 0;
+                              if(temp > 0) {
+                                controller.total_unread_messages.value = (controller.total_unread_messages.value - temp).toInt();
+                              }
+                              /*controller.users.clear();
+                              controller.socket.close();
+                              controller.connect();*/
+
+
+                              print('CALL BACK END CALL }${controller.total_unread_messages.value}');
+
+
+                            },
+                        ));
                       }, // get last message item
                       child: getMainChatItem(controller.users[i]),
                     ),
