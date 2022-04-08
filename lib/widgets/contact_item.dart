0bc74e829c5b16926eee8721b6bf14c144cf1785
HimanshuGandhi <himanshu.g@crestinfosystems.net>
2022-04-08 12:08:06 +0530

    Change UI as per designer request

--- lib/widgets/contact_item.dart
+++ lib/widgets/contact_item.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plug/app/modules/contact/model/pluhg_contact.dart';
 import 'package:plug/app/values/colors.dart';
+import 'package:plug/widgets/radio_list_widget.dart';
 import 'package:plug/widgets/text_style.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 Widget contactItem(PluhgContact contact, Function()? onTap) {
@@ -16,17 +17,7 @@ Widget contactItem(PluhgContact contact, Function()? onTap) {
   temString.addAll(contact.phoneNumbers.take(2).map((e) => ContactItemDataClass(value: e,type: contactItemType.phone)).toList());
   temString.addAll(contact.emailAddresses.take(2).map((e) => ContactItemDataClass(value: e,type: contactItemType.email)).toList());
 
-
-  //int? radioGroupValue = contact.selectedContact != null ? temString.indexOf(contact.selectedContact!): null;
-  int? radioGroupValue = contact.selectedContact != null ? temString.indexOf(temString.firstWhere((e) => e.value == contact.selectedContact!)): null;
-
   ContactItemDataClass? newRadioGroupValue = contact.selectedContact != null ? temString.firstWhere((element) => element.value == contact.selectedContact) : null;
-
-  print('------------------------------------------ START');
-
-  temString.forEach((element) {
-    print('------------------data-------$element');
-  });
 
   return InkWell(
     onTap: onTap,
@@ -35,82 +26,108 @@ Widget contactItem(PluhgContact contact, Function()? onTap) {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
               _avatar(contact.name.substring(0, 1), contact.photo, contact.isPlughedUser),
               SizedBox(
                 width: 12,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    contact.name,
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black),
-                  ),
-
-                 /* if(contact.phoneNumbers.length <= 1 && contact.emailAddresses.length <=1)
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            contact.phoneNumber.isEmpty
-                                ? contact.emailAddress
-                                : contact.phoneNumber,
-                            style: TextStyle(
-                                fontSize: 12.sp,
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            (contact.emailAddress.isEmpty || contact.phoneNumber.isEmpty)
-                                ? ""
-                                : contact.emailAddress,
-                            style: TextStyle(
-                                fontSize: 11.sp,
-                                fontWeight: FontWeight.w400,
-                                color: AppColors.pluhgMenuGrayColour),
-                          ),
-                        ],
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      contact.name,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
                       ),
+                    ),
 
-                  if(contact.phoneNumbers.length > 1 || contact.emailAddresses.length > 1)*/
-                      Column(
-                        children: temString.map((e) => Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                   /* if(contact.phoneNumbers.length <= 1 && contact.emailAddresses.length <=1)
+                        Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Container(
-                              width: 220.w,
-                              child: RadioListTile<ContactItemDataClass?>(
-                                title: Text(e.value),
-                                dense: true,
-                                visualDensity: VisualDensity.compact,
-                                groupValue: newRadioGroupValue,
-                                value: e,
-                                activeColor: AppColors.pluhgColour,
-                                onChanged: (value) async{
-                                  print(value);
-                                  newRadioGroupValue = value;
-                                  if(value?.type == contactItemType.phone){
-                                        contact.selectedContact =contact.phoneNumbers.firstWhere((element) => element == e.value);
-                                      } else {
-                                        contact.selectedContact =contact.emailAddresses.firstWhere((element) => element == e.value);
-                                      }
-                                  onTap!();
-                                      /*if(value == 0 || value == 1){
-                                    contact.selectedContact = contact.phoneNumbers.firstWhere((element) => element == e);
-                                  }else{
-                                    contact.selectedContact = contact.emailAddresses.firstWhere((element) => element == e);
-                                  }
-                                  onTap!();*/
-                                },),),
+                            Text(
+                              contact.phoneNumber.isEmpty
+                                  ? contact.emailAddress
+                                  : contact.phoneNumber,
+                              style: TextStyle(
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black),
+                            ),
+                            Text(
+                              (contact.emailAddress.isEmpty || contact.phoneNumber.isEmpty)
+                                  ? ""
+                                  : contact.emailAddress,
+                              style: TextStyle(
+                                  fontSize: 11.sp,
+                                  fontWeight: FontWeight.w400,
+                                  color: AppColors.pluhgMenuGrayColour),
+                            ),
                           ],
-                        )).toList(),
-                      ),
-                ],
+                        ),
+
+                    if(contact.phoneNumbers.length > 1 || contact.emailAddresses.length > 1)*/
+
+                    Column(
+                      children: temString.map((e) => RadioListTitleComponent<ContactItemDataClass?>(
+                        label: e.value,
+                        value: e,
+                        isChecked: contact.selectedContact == e.value,
+                        itemSelected: (value){
+                          print(value);
+
+                          if(contact.selectedContact != null)
+                            return;
+
+                          newRadioGroupValue = value;
+                          if(value?.type == contactItemType.phone){
+                            contact.selectedContact =contact.phoneNumbers.firstWhere((element) => element == e.value);
+                          }
+                          else {
+                            contact.selectedContact =contact.emailAddresses.firstWhere((element) => element == e.value);
+                          }
+                          onTap!();
+                        },
+                      )
+                      ).toList(),
+                    ),
+                        /*Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: temString.map((e) => Container(
+                            width: 220.w,
+                            child: RadioListTile<ContactItemDataClass?>(
+                              title: Text(e.value),
+                              dense: true,
+                              tileColor: Colors.red,
+                              contentPadding: EdgeInsets.zero,
+                              visualDensity: const VisualDensity(
+                                horizontal: 0,
+                                vertical: 0,
+                              ),
+                              groupValue: newRadioGroupValue,
+                              controlAffinity: ListTileControlAffinity.leading,
+                              value: e,
+                              activeColor: AppColors.pluhgColour,
+                              onChanged: (value) async{
+                                print(value);
+                                newRadioGroupValue = value;
+                                if(value?.type == contactItemType.phone){
+                                      contact.selectedContact =contact.phoneNumbers.firstWhere((element) => element == e.value);
+                                    }
+                                else {
+                                      contact.selectedContact =contact.emailAddresses.firstWhere((element) => element == e.value);
+                                    }
+                                onTap!();
+                              },),)).toList(),
+                        ),*/
+                  ],
+                ),
               ),
             ],
           ),
