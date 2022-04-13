d8e59b959f6e034d5621889f8d6bb66000dd8b9a
HimanshuGandhi <himanshu.g@crestinfosystems.net>
2022-04-13 09:59:09 +0530

    fix null (user name) in chat screen

--- lib/app/modules/chat_screen/controllers/chat_screen_controller.dart
+++ lib/app/modules/chat_screen/controllers/chat_screen_controller.dart
@@ -104,7 +104,7 @@ class ChatScreenController extends GetxController {
     if (name.isEmpty) {
       users.value = usersTemp;
     } else {
-      users.value = usersTemp.where((element) => element.name.toLowerCase().contains(name.toLowerCase())).toList();
+      users.value = usersTemp.where((element) => element.name != null ? element.name!.toLowerCase().contains(name.toLowerCase()) : element.userName.toLowerCase().contains(name.toLowerCase())).toList();
     }
   }
 }
